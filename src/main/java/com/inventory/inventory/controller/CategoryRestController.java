e9eef89208a3acff666e9dff07fcf8b3291c618a
draquio <draquio@gmail.com>
2024-02-21 16:45:05 -0400

    Servicio de listar categorias por Id funcionando

--- src/main/java/com/inventory/inventory/controller/CategoryRestController.java
+++ src/main/java/com/inventory/inventory/controller/CategoryRestController.java
@@ -4,6 +4,7 @@ import com.inventory.inventory.services.ICategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,9 +13,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoryRestController {
     @Autowired
     private ICategoryService service;
+
+    // Get all categories
     @GetMapping("/categories")
     public ResponseEntity<CategoryResponseRest> searchCategories(){
         ResponseEntity<CategoryResponseRest> response = service.search();
+        return response;
+    }
+
+    // Get category by Id
+    @GetMapping("/categories/{id}")
+    public ResponseEntity<CategoryResponseRest> searchCategoriesById(@PathVariable Long id){
+        ResponseEntity<CategoryResponseRest> response = service.searchById(id);
         return response;
     }
 }
